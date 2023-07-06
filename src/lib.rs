2ee0b5d7bde5f4a15d976c5d691df04735142e2e
Matthew Gapp <61894094+matthewgapp@users.noreply.github.com>
2023-07-05 23:22:20 -0700

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,256 @@
+use rsb_derive::Builder;
+use serde::Serialize;
+use ts_rs::TS;
+
+#[derive(Debug, Clone, Serialize, TS)]
+#[ts(export)]
+pub struct Location<C> {
+    id: Option<u32>,
+    txn_id: Option<u32>,
+    collection: C,
+}
+
+// #[derive(Debug, Clone, Serialize, TS)]
+// #[ts(export)]
+// struct EphemeralLocation<C> {
+//     txn_id: Option<u32>,
+//     collection: C,
+// }
+
+// struct
+
+#[derive(Debug, Clone, Serialize, Builder, TS)]
+#[ts(export)]
+pub struct UpdatableResource<T, C>
+where
+    T: TS,
+{
+    location: Location<C>,
+    data: T,
+}
+
+#[derive(Debug, Clone, Serialize, Builder, TS)]
+#[ts(export)]
+pub struct AppendableResource<T, C>
+where
+    T: TS,
+{
+    location: Location<C>,
+    data: T,
+}
+
+#[derive(Debug, Clone, Serialize, TS)]
+#[ts(export)]
+pub struct ResourceId(u32);
+
+#[derive(Debug, Clone, Serialize, TS)]
+// this produces a json object with a "type" field and a "payload" field
+#[serde(tag = "type", content = "payload", rename_all = "snake_case")]
+#[ts(export)]
+pub enum EventVerb<T: Serialize + TS, C> {
+    Insert(AppendableResource<T, C>),
+    Update(UpdatableResource<T, C>),
+    Upsert(UpdatableResource<T, C>),
+    Delete(ResourceId),
+}
+
+#[derive(Debug, Clone, Serialize, TS)]
+#[ts(export)]
+pub struct Event<T: Serialize + TS, C> {
+    verb: EventVerb<T, C>,
+}
+
+impl<T: Serialize + TS, C> Event<T, C> {
+    pub fn new(verb: EventVerb<T, C>) -> Self {
+        Self { verb }
+    }
+}
+
+#[derive(Serialize)]
+pub struct WsBody<T: Serialize> {
+    data: T,
+}
+
+impl<T: Serialize> WsBody<T> {
+    pub fn new(data: T) -> Self {
+        Self { data }
+    }
+
+    pub fn json(&self) -> String {
+        serde_json::to_string(&self).expect("Could not serialize WsBody<T> to JSON")
+    }
+}
+
+// trait PersistedLocatable {
+//     fn id(&self) -> u32;
+
+//     fn location(&self) -> Location<Self::Collection> {
+//         Location {
+//             id: self.id(),
+//             txn_id: None,
+//             collection: self.collection(),
+//         }
+//     }
+// }
+
+pub trait Appendable: Serialize + Sized + TS {
+    type Collection;
+
+    fn collection(&self) -> Self::Collection;
+
+    fn to_insert_event(self) -> Event<Self, Self::Collection> {
+        <Self as Appendable>::to_event(self, EventVerb::Insert)
+    }
+
+    fn to_event(
+        self,
+        build_verb: impl FnOnce(
+            AppendableResource<Self, Self::Collection>,
+        ) -> EventVerb<Self, Self::Collection>,
+    ) -> Event<Self, Self::Collection> {
+        let location = Location {
+            id: None,
+            txn_id: None,
+            collection: self.collection(),
+        };
+        let verb = build_verb(AppendableResource::new(location, self));
+        Event::new(verb)
+    }
+}
+
+pub trait Syncable: Appendable {
+    fn id(&self) -> u32;
+
+    fn to_upsert_event(self) -> Event<Self, Self::Collection> {
+        <Self as Syncable>::to_event(self, EventVerb::Upsert)
+    }
+
+    fn to_update_event(self) -> Event<Self, Self::Collection> {
+        <Self as Syncable>::to_event(self, EventVerb::Update)
+    }
+
+    fn to_event(
+        self,
+        build_verb: impl FnOnce(
+            UpdatableResource<Self, Self::Collection>,
+        ) -> EventVerb<Self, Self::Collection>,
+    ) -> Event<Self, Self::Collection> {
+        let location = Location {
+            id: None,
+            txn_id: None,
+            collection: self.collection(),
+        };
+        let verb = build_verb(UpdatableResource::new(location, self));
+        Event::new(verb)
+    }
+}
+
+mod test {
+    use serde::Serialize;
+    use ts_rs::TS;
+
+    use crate::{Appendable, Location, Syncable};
+
+    #[derive(Serialize, TS)]
+    struct DoggoRecord {
+        id: u32,
+        name: String,
+        breed: String,
+    }
+
+    #[allow(dead_code)]
+    #[derive(Serialize, TS)]
+    enum Collection {
+        Dogs,
+        Cats,
+    }
+
+    impl Appendable for DoggoRecord {
+        type Collection = Collection;
+
+        fn collection(&self) -> Self::Collection {
+            Collection::Dogs
+        }
+    }
+
+    impl Syncable for DoggoRecord {
+        fn id(&self) -> u32 {
+            self.id
+        }
+    }
+
+    #[test]
+    fn conversion_works() {
+        use super::*;
+
+        let doggo = DoggoRecord {
+            id: 1,
+            name: "Barky".to_string(),
+            breed: "Poodle".to_string(),
+        };
+
+        let event = doggo.to_insert_event();
+        let json = WsBody::new(event).json();
+        insta::assert_snapshot!(json, @r###"{"data":{"verb":{"type":"insert","payload":{"location":{"id":1,"txn_id":null,"collection":"Dogs"},"data":{"id":1,"name":"Barky","breed":"Poodle"}}}}}"###);
+    }
+}
+
+#[async_trait::async_trait]
+pub trait Listener {
+    type Error;
+    type Item;
+
+    async fn recv(&mut self) -> Result<Self::Item, Self::Error>;
+}
+
+pub trait Service {
+    type Item: Appendable;
+    type Listener: Listener<Item = Self::Item>;
+
+    fn publish(&self, event: Self::Item) -> Result<(), ()>;
+    fn listener(&self) -> Self::Listener;
+}
+
+// pub struct Service {
+//     ch_file_event: broadcast::Sender<FileEvent>,
+// }
+
+// impl Service {
+//     pub fn new() -> Self {
+//         let (tx, _) = broadcast::channel(100);
+//         Self { ch_file_event: tx }
+//     }
+
+//     fn file_event_sender(&self) -> &broadcast::Sender<FileEvent> {
+//         &self.ch_file_event
+//     }
+
+//     pub fn listener_file_event(&self) -> Listener<FileEvent> {
+//         let ch = self.ch_file_event.subscribe();
+//         Listener::new(ch)
+//     }
+
+//     pub fn file_uploaded(
+//         &self,
+//         file_name: &str,
+//         id: u32,
+//     ) -> Result<usize, broadcast::error::SendError<FileEvent>> {
+//         self.file_event_sender()
+//             .send(FileEvent::new(FileStatus::Uploaded, file_name, id))
+//     }
+
+//     pub fn file_embedded(
+//         &self,
+//         file_name: &str,
+//         id: u32,
+//     ) -> Result<usize, broadcast::error::SendError<FileEvent>> {
+//         self.file_event_sender()
+//             .send(FileEvent::new(FileStatus::Embedded, file_name, id))
+//     }
+// }
+
+// impl Default for Service {
+//     fn default() -> Self {
+//         Self::new()
+//     }
+// }
